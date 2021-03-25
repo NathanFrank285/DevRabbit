13e321465fba619f0e0452ff210ac67d80cad02b
NathanFrank285 <nathansfrank@yahoo.com>
2021-03-25 09:27:26 -0700

    users can login

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,28 +1,9 @@
-const router = require('express').Router()
-const asyncHandler = require("express-async-handler");
-const { setTokenCookie, restoreUser, requireAuth } = require("../../utils/auth.js");
-const { User } = require("../../db/models");
+const router = require("express").Router();
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
 
-//* testing auth routes
-// router.get("/restore-user", restoreUser, (req, res) => {
-//   return res.json(req.user);
-// });
+router.use("/session", sessionRouter);
 
-// router.get("/require-auth", requireAuth, (req, res) => {
-//   return res.json(req.user);
-// });
-// router.get(
-//   "/set-token-cookie",
-//   asyncHandler(async (req, res) => {
-//     const user = await User.findOne({
-//       where: {
-//         username: "Demo-lition",
-//       },
-//     });
-//     setTokenCookie(res, user);
-//     return res.json({ user });
-//   })
-// );
+router.use("/users", usersRouter);
 
-
-module.exports = router
+module.exports = router;
