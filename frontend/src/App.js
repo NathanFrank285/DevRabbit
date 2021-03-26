c55039ae0fef34103936dfccccae0f91009bf3a5
NathanFrank285 <nathansfrank@yahoo.com>
2021-03-26 13:11:38 -0700

    saving for heroku

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
-
+import SignupFormPage from './components/SignupFormPage/index'
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
-import SignupFormPage from "./components/SignupFormPage";
+
 
 function App() {
   const dispatch = useDispatch();
