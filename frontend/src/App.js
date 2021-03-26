c513e68f9347f49ad8d77537ff8f167a1fbd954c
NathanFrank285 <nathansfrank@yahoo.com>
2021-03-26 13:15:23 -0700

    edited signUpformpage path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
-import SignupFormPage from './components/SignupFormPage/index'
+import SignupFormPage from "/Users/nathanfrank/Desktop/App_Academy/Projects/authenticate-me/frontend/src/components/SignupFormPage/index.js";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 
